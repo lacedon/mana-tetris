7c9fcb2231728ecc9102d3060161bf2915a0e51d
Viktor Kukharev <lacedon1@gmail.com>
2025-07-22 21:09:32 -0400

    Add drop figure controll

--- src/components/controller/controller.gd
+++ src/components/controller/controller.gd
@@ -8,7 +8,9 @@ func _input(event: InputEvent) -> void:
     if event.is_action_pressed("rotate"):
         figureMover.rotate_figure()
     elif event.is_action_pressed("drop"):
-        figureMover.drop_figure()
+        figureMover.toggle_figure_dropping(true)
+    elif event.is_action_released("drop"):
+        figureMover.toggle_figure_dropping(false)
     elif event.is_action_pressed("move-left"):
         figureMover.move_figure(-1)
     elif event.is_action_pressed("move-right"):
--- src/components/field/figure_mover/figure_mover.gd
+++ src/components/field/figure_mover/figure_mover.gd
@@ -3,26 +3,32 @@ extends Node
 const FigureNode = preload("res://src/components/figure/figure.gd")
 
 @export var figureNode: FigureNode
-@export var speed: float = 1.0
+@export var movingWaitTime: float = 1.0
+@export var movingWaitTimeFast: float = 0.1
 @export var isPaused: bool = false
 
 @onready var timer: Timer = $Timer
 
+var isFastMoving: bool = false
+
 func _ready() -> void:
-    timer.start()
+    set_up_timer()
 
 func _on_timer_timeout() -> void:
     if figureNode && !isPaused:
         figureNode.move_down_figure()
 
+func set_up_timer(wait_time: float = movingWaitTime) -> void:
+    timer.wait_time = wait_time
+    timer.start()
+
 func rotate_figure() -> void:
     if figureNode:
         figureNode.rotate_figure()
 
-func drop_figure() -> void:
-    if figureNode:
-        # TODO: Implement drop logic
-        pass
+func toggle_figure_dropping(isFastMovingEnabled) -> void:
+    isFastMoving = isFastMovingEnabled
+    set_up_timer(movingWaitTimeFast if isFastMoving else movingWaitTime)
 
 func move_figure(direction: int) -> void:
     if figureNode:
