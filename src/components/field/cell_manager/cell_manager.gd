118f868787328b17cd98c0a2ebc3aea6e50ffc60
Viktor Kukharev <lacedon1@gmail.com>
2025-07-22 21:09:48 -0400

    Fix connecting to figure_set event

--- src/components/field/cell_manager/cell_manager.gd
+++ src/components/field/cell_manager/cell_manager.gd
@@ -5,10 +5,10 @@ const FigureNode = preload("res://src/components/figure/figure.gd")
 @export var figureNode: FigureNode
 
 func _ready() -> void:
-    self.connect(figureNode.figure_set.get_name(), _set_figure_cells)
+    figureNode.connect(figureNode.figure_set.get_name(), _set_figure_cells)
 
 func _exit_tree() -> void:
-    self.disconnect(figureNode.figure_set.get_name(), _set_figure_cells)
+    figureNode.disconnect(figureNode.figure_set.get_name(), _set_figure_cells)
 
 func _set_figure_cells(figure: GameFigure) -> void:
     prints('Setting figure cells for: ', figure)
