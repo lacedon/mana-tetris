82047f0fa36109639a0d9e32869673d234c4dd5f
Viktor Kukharev <lacedon1@gmail.com>
2025-08-19 20:50:01 -0400

    Rework cells to be collision objects

--- src/components/cell/cell.gd
+++ src/components/cell/cell.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CollisionShape2D
 
 const CellTypes = preload("res://src/types/cell_types.gd").CellTypes
 const CellVisual = preload("res://src/components/cell/cell_visual/cell_visual.gd")
@@ -6,38 +6,33 @@ const CellCollisionStatic = preload("res://src/components/cell/cell_collision_st
 const CellCollisionStaticScene = preload("res://src/components/cell/cell_collision_static/cell_collision_static.tscn")
 
 enum CellRenderMode {
-    STATIC,
-    VISUAL_ONLY,
+	STATIC,
+	VISUAL_ONLY,
 }
 
 signal cell_ready
 
 @export var cell_type: CellTypes = CellTypes.EMPTY
-@export var render_mode: CellRenderMode = CellRenderMode.STATIC
 
 @onready var cell_visual: CellVisual = $CellVisual
-
-var collision: CollisionObject2D
+@onready var collision: CollisionShape2D = self
 
 func _ready() -> void:
-    _prepare_collision()
-    set_cell_type()
-    emit_signal(cell_ready.get_name())
-    cell_visual.init(cell_type, FieldConfig.cell_size)
+	_prepare_collision()
+	set_cell_type()
+	emit_signal(cell_ready.get_name())
+	cell_visual.init(cell_type, FieldConfig.cell_size)
 
 func set_cell_type(new_type: CellTypes = cell_type) -> void:
-    cell_type = new_type
-    visible = cell_type != CellTypes.EMPTY
-    if (cell_visual): cell_visual.init(new_type)
-
-func set_render_mode(new_mode: CellRenderMode) -> void:
-    render_mode = new_mode
-    _prepare_collision()
+	cell_type = new_type
+	visible = cell_type != CellTypes.EMPTY
+	if (cell_visual): cell_visual.init(new_type)
 
 func _prepare_collision() -> void:
-    if collision: collision.queue_free()
-
-    if render_mode == CellRenderMode.STATIC:
-        var staticCollision: CellCollisionStatic = CellCollisionStaticScene.instantiate() as CellCollisionStatic
-        add_child(staticCollision)
-        collision = staticCollision
+	var collision_shape: ConvexPolygonShape2D = collision.shape
+	collision_shape.points = [
+		Vector2.ZERO,
+		Vector2(0, FieldConfig.cell_size.y),
+		Vector2(FieldConfig.cell_size.x, FieldConfig.cell_size.y),
+		Vector2(FieldConfig.cell_size.x, 0),
+	]
--- src/components/figure/cell_manager/cell_manager.gd
+++ src/components/figure/cell_manager/cell_manager.gd
@@ -6,50 +6,52 @@ const CellNode = preload("res://src/components/cell/cell.gd")
 const EntityPool = preload("res://src/components/entity_pool/entity_pool.gd")
 
 @export var default_figure_area: int = 2
+@export var cell_container: Node
 
 @onready var entity_pool: EntityPool = $EntityPool
-@onready var cell_container: Node = $Cells
+@onready var default_cell_container: Node = $Cells
 
 func _ready() -> void:
-    entity_pool.init(
-        _create_cell,
-        default_figure_area,
-        EntityPool.IncreasingMode.FACTORIAL,
-        cell_container
-    )
+	if !cell_container: cell_container = default_cell_container
+
+	entity_pool.init(
+		_create_cell,
+		default_figure_area,
+		EntityPool.IncreasingMode.FACTORIAL,
+		cell_container
+	)
 
 func _create_cell(index: int) -> CellNode:
-    var cell_instance: CellNode = CellScene.instantiate() as CellNode
-    cell_instance.name = "Cell#" + str(index)
-    cell_instance.set_render_mode(CellNode.CellRenderMode.VISUAL_ONLY)
-    return cell_instance
+	var cell_instance: CellNode = CellScene.instantiate() as CellNode
+	cell_instance.name = "Cell#" + str(index)
+	return cell_instance
 
 func _get_cell_position(rotation_mode: int, index: int, figure_size: Vector2) -> Vector2:
-    var x: int = index % int(figure_size.x)
-    var y: int = floor(index / figure_size.x)
-    if rotation_mode == 1: # 90 degrees
-        return Vector2(y, figure_size.x - 1 - x)
-    elif rotation_mode == 2: # 180 degrees
-        return Vector2(figure_size.x - 1 - x, figure_size.y - 1 - y)
-    elif rotation_mode == 3: # 270 degrees
-        return Vector2(figure_size.y - 1 - y, x)
-    else: # 0 degrees
-        return Vector2(x, y)
+	var x: int = index % int(figure_size.x)
+	var y: int = floor(index / figure_size.x)
+	if rotation_mode == 1: # 90 degrees
+		return Vector2(y, figure_size.x - 1 - x)
+	elif rotation_mode == 2: # 180 degrees
+		return Vector2(figure_size.x - 1 - x, figure_size.y - 1 - y)
+	elif rotation_mode == 3: # 270 degrees
+		return Vector2(figure_size.y - 1 - y, x)
+	else: # 0 degrees
+		return Vector2(x, y)
 
 func set_up_cells(rotation_mode: int, figure: GameFigure) -> void:
-    var figure_area: int = figure.get_area()
+	var figure_area: int = figure.get_area()
 
-    for index in range(figure_area):
-        var cell: CellNode = entity_pool.get_cell_by_index(index, true)
-        var cell_type: CellTypes = figure.get_cell_type(index)
-        cell.set_cell_type(cell_type)
-        cell.position = _get_cell_position(rotation_mode, index, figure.size) * FieldConfig.cell_size
+	for index in range(figure_area):
+		var cell: CellNode = entity_pool.get_cell_by_index(index, true)
+		var cell_type: CellTypes = figure.get_cell_type(index)
+		cell.set_cell_type(cell_type)
+		cell.position = _get_cell_position(rotation_mode, index, figure.size) * FieldConfig.cell_size
 
-    # Remove unused cells
-    for index in range(entity_pool.get_used_entities_count() - 1, figure_area - 1, -1):
-        entity_pool.remove_entity_by_index(index)
+	# Remove unused cells
+	for index in range(entity_pool.get_used_entities_count() - 1, figure_area - 1, -1):
+		entity_pool.remove_entity_by_index(index)
 
 func get_cells() -> Array[CellNode]:
-    var cells: Array[CellNode]
-    cells.assign(entity_pool.get_used_entities())
-    return cells
+	var cells: Array[CellNode]
+	cells.assign(entity_pool.get_used_entities())
+	return cells
